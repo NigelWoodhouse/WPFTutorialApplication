07b05fd7c226cb66204cb2d6796889db73902aa2
Nigel Woodhouse <njwoodhouse@gmail.com>
2022-09-15 23:10:03 -0600

    Add project files.

---
+++ WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
+
+namespace WiredBrainCoffee.CustomersApp.View
+{
+    /// <summary>
+    /// Interaction logic for CustomersView.xaml
+    /// </summary>
+    public partial class CustomersView : UserControl
+    {
+        private CustomersViewModel _viewModel;
+
+        public CustomersView()
+        {
+            InitializeComponent();
+            _viewModel = new CustomersViewModel(new CustomerDataProvider());
+            DataContext = _viewModel;
+            Loaded += CustomersView_Loaded;
+        }
+
+        private async void CustomersView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
+        }
+    }
+}
---
+++ WiredBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using WiredBrainCoffee.CustomersApp.Command;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class CustomersViewModel : ViewModelBase
+    {
+        private readonly ICustomerDataProvider _customerDataProvider;
+
+
+        private CustomerItemViewModel? _selectedCustomer;
+        private NavigationSide _navigationSide;
+
+        public CustomersViewModel(ICustomerDataProvider customerDataProvider)
+        {
+            _customerDataProvider = customerDataProvider;
+            AddCommand = new DelegateCommand(Add);
+            MoveNavigationCommand = new DelegateCommand(MoveNavigation);
+            DeleteCommand = new DelegateCommand(Delete, CanDelete);
+        }
+
+
+
+        public ObservableCollection<CustomerItemViewModel> Customers { get; } = new();
+
+        public CustomerItemViewModel? SelectedCustomer
+        {
+            get => _selectedCustomer;
+            set
+            {
+                _selectedCustomer = value;
+                RaisePropertyChanged();
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public async Task LoadAsync()
+        {
+            if (Customers.Any())
+            {
+                return;
+            }
+
+            var customers = await _customerDataProvider.GetAllAsync();
+            if (customers is not null)
+            {
+                foreach (var customer in customers)
+                {
+                    Customers.Add(new CustomerItemViewModel(customer));
+                }
+            }
+        }
+
+        public NavigationSide NavigationSide
+        {
+            get => _navigationSide;
+            private set
+            {
+                _navigationSide = value;
+                RaisePropertyChanged(nameof(NavigationSide));
+            }
+        }
+
+        public DelegateCommand AddCommand { get; }
+
+        public DelegateCommand MoveNavigationCommand { get; }
+
+        public DelegateCommand DeleteCommand { get; }
+
+
+        private void Add(object? paramenter)
+        {
+            var customer = new Customer { FirstName = "New" };
+            var viewModel = new CustomerItemViewModel(customer);
+            Customers.Add(viewModel);
+            SelectedCustomer = viewModel;
+        }
+
+        private void Delete(object? paramenter)
+        {
+            if(SelectedCustomer is not null)
+            {
+                Customers.Remove(SelectedCustomer);
+                SelectedCustomer = null;
+            }
+        }
+
+        private bool CanDelete(object? paramenter) => SelectedCustomer is not null;
+
+        private void MoveNavigation(object? paramenter)
+        {
+            NavigationSide = NavigationSide == NavigationSide.Left
+                ? NavigationSide.Right
+                : NavigationSide.Left;
+        }
+    }
+
+    public enum NavigationSide
+    {
+        Left,
+        Right,
+    }
+}
