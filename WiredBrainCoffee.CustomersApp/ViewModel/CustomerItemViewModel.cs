8bc8cb49655c7fc6eb125e5ecd8bbec3d0e4a52d
Nigel Woodhouse <njwoodhouse@gmail.com>
2022-09-18 21:50:53 -0600

    Completed Tutorial

--- WiredBrainCoffee.CustomersApp/ViewModel/CustomerItemViewModel.cs
+++ WiredBrainCoffee.CustomersApp/ViewModel/CustomerItemViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace WiredBrainCoffee.CustomersApp.ViewModel
 {
-    public class CustomerItemViewModel : ViewModelBase
+    public class CustomerItemViewModel : ValidationViewModelBase
     {
         private readonly Customer _model;
 
@@ -19,6 +19,13 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
             get => _model.FirstName;
             set { _model.FirstName = value;
                 RaisePropertyChanged();
+                if (string.IsNullOrEmpty(_model.FirstName))
+                {
+                    AddError("Firstname is required");
+                } else
+                {
+                    ClearErrors();
+                }
             }
         }
 
